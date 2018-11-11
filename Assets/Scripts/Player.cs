b84b1a7696c4292410643f5c9ffd4b7c85eaea6d
Aleksander Kania <kania.alek@gmail.com>
2018-11-11 13:58:43 +0100

    Add move to cube

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed;
+
+	void Start () {
+		
+		maxSpeed = 10f;
+	}
+	
+	void Update () {
+
+		var totalSpeed = Time.deltaTime * maxSpeed;
+
+		transform.Translate(
+			Input.GetAxis("Horizontal") * totalSpeed,
+			 0f,
+			Input.GetAxis("Vertical") * totalSpeed
+		);
+	}
+}
