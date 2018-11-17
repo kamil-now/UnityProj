b80717f529d04b7debf22b1f6a6cdbaa8b599a7e
Kamil <k.nowak3003@gmail.com>
2018-11-17 13:48:36 +0100

    move controls

---
+++ Assets/Scripts/MoveControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MoveControls : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float tilt;
+    private float horizontal;
+    private float vertical;
+
+    private new Rigidbody rigidbody;
+    private Vector3 vector;
+
+    #region MonoBehaviour
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        speed = 1000;
+    }
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        vector = new Vector3(horizontal, 0, vertical);
+
+        rigidbody.velocity = vector * Time.fixedUnscaledDeltaTime * speed;
+
+        //rigidbody.position = new Vector3(
+        //    Mathf.Clamp(rigidbody.position.x, GameManager.Instance.LeftBorder, GameManager.Instance.RightBorder),
+        //    Values.InitPlayerPosition.y,
+        //    Mathf.Clamp(rigidbody.position.z, GameManager.Instance.BottomBorder, GameManager.Instance.TopBorder));
+
+    }
+    #endregion
+}
+
