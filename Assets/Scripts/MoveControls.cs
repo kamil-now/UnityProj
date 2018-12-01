3ad58056a13ac486610f42ef5a279514529bde7b
Kamil <k.nowak3003@gmail.com>
2018-12-01 09:48:42 +0100

    player movement script and animations

---
+++ Assets/Scripts/MoveControls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveControls : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        speed = 5;
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        if (horizontal > 0.5f || horizontal < -0.5f )
+        {
+            transform.Translate(new Vector3(horizontal * speed * Time.deltaTime, 0f, 0f));
+        }
+        if (vertical > 0.5f || vertical < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, vertical * speed * Time.deltaTime, 0f));
+        }
+        animator.SetFloat("MoveX", horizontal);
+        animator.SetFloat("MoveY", vertical);
+    }
+}
