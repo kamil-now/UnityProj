31abb3b8949130754e6c6ef66dd9d4b833b6ef42
Aleksander <kania.alek@gmail.com>
2018-12-15 11:09:42 +0100

    Add walls

---
+++ Assets/Collision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("dupa");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+    }
+}
